884845417a56d000e2f83b54c551614c5412430f
Poon <poon.wuthi@gmail.com>
2016-09-26 18:12:17 +0700

    Fixed to return dictionary

--- categorizer.py
+++ categorizer.py
@@ -16,29 +16,16 @@ def containKeywords(keywords, text):
     return i
 
 def categorize(input, dictionary):
-    data = dictionary
     scorer = {}
-    m = 0 #max
-    mt = 0 #total
-    mk = None
+    total = 0
 
-    print('Scoring dictionary')
-    for k, v in data.items():
+    # iterate and score dictionary
+    for k, v in dictionary.items():
         scorer[k] = containKeywords(v, input)
-        if scorer[k] > m:
-            m = scorer[k]
-            mt += scorer[k]
-            mk = k
-    return mk, m / float(mt)
+        total += scorer[k]
 
-if __name__ == '__main__':
-    # default command
-    dictionary = 'sample/mapper.json'
-    with open(dictionary, encoding='utf-8') as mapper:
-        print('Parsing dictionary %s' % dictionary)
-        data = json.load(mapper)
-    mk,conf = categorize('beauty', data)
-    if mk is None:
-        print("No category")
-    else:
-        print("category - %s\nconfidence - %.4f" % (mk.encode('utf-8'), conf))
+    # get confidence value
+    for k, v in scorer.items():
+        scorer[k] = scorer[k] / float(total)
+
+    return scorer
